4b2120cbe1b288d43662994263438abb3ec8f460
Niyazov Sarvarjon <sarvarjonniyazov833@gmail.com>
2026-02-04 20:20:32 +0900

    UI layout has been created

---
+++ src/components/dashboard/dashboard/navbar.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function Navbar() {
+  return (
+    <div className="border-b">
+      <nav className="flex items-center justify-between p-4 h-16">
+        <h4 className="font-semibold">Dashboard</h4>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Sarvarjon</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuSeparator />
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Dashboard } from './components/dashboard/layout/dashboard-layout.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1 className="text-5xl">hello world</h1>,
+  },
+  {
+    path: 'login',
+    element: <h1 className="text-5xl">Login</h1>,
+  },
+  {
+    path: 'register',
+    element: <h1 className="text-5xl">Register</h1>,
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />,
+    children: [
+      {
+        index: true,
+        element: <h1>Dashboard main page</h1>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
